117551053c42368a9151afb7fd35f88fd558b02e
Thomas Wade <gigatech55@gmail.com>
2019-09-13 13:13:20 +0930

    Move code definitions to the main file

--- remote-control/receiver.c
+++ remote-control/receiver.c
@@ -23,17 +23,6 @@ void send_code(byte code);
 #define BUTTON_LOW 1
 #define BUZZER PORTCbits.RC1
 
-// Code definitions
-// Only the last 3 bits are used
-//                 XXXXX---
-#define CODE_NOP 0b00000000 // No-op
-#define CODE_VUP 0b00000001 // Volume up
-#define CODE_VDN 0b00000010 // Volume down
-#define CODE_VMT 0b00000011 // Mute/unmute
-#define CODE_CUP 0b00000100 // Channel up
-#define CODE_CDN 0b00000101 // Channel down
-#define CODE_CRS 0b00000110 // Channel reset
-
 int volume;
 int channel;
 
